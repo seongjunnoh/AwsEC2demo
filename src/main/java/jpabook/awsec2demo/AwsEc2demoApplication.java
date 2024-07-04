ad43fc01d36cfb98bbdfc035d45bd18ebf46251b
seongjunnoh <seongjunnoh@seongjunnohui-noteubug.local>
2024-07-04 13:03:59 +0900

    first commit

---
+++ src/main/java/jpabook/awsec2demo/AwsEc2demoApplication.java
@@ -0,0 +1,13 @@
+package jpabook.awsec2demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsEc2demoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsEc2demoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jpabook/awsec2demo/AwsEc2demoApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabook.awsec2demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsEc2demoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
